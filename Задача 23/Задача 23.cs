c40cae4447c350d9156be1418bf6981b92293b72
lightlana <lebendaufdensternen@gmail.com>
2022-10-02 15:18:44 +0300

    Программа, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.

---
+++ Задача 23/Задача 23.cs
@@ -0,0 +1,12 @@
+﻿ void task23()
+ {
+    Console.WriteLine("Число N");
+    int number = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Число\tКуб");
+    for (int i=1; i <= number; i++)
+    {
+        Console.WriteLine("{0}\t{1}\t", i, Math.Pow(i,3));
+    }
+
+ }
+ task23();
