3a15bedd3a5fc62c1bf8ccde19573da09460f8dc
lightlana <lebendaufdensternen@gmail.com>
2022-10-03 22:26:31 +0300

    Программа, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом

--- Задача 19/Задача 19.cs
+++ Задача 19/Задача 19.cs
@@ -1,21 +1,32 @@
-﻿        int num, rem, sum = 0, temp;
+﻿    void task19() 
+    {   
+        int num, rem, sum = 0, temp;
         Console.Write("Введите число: ");
         num = Convert.ToInt32(Console.ReadLine());
         temp = num;
-        while (num > 0)
+        if (num > 9999 && num < 100000)
         {
-            rem = num % 10;  
-            num = num / 10; 
-            sum = sum * 10 + rem; 
-  
-        }
+            while (num > 0)
+            {
+                rem = num % 10;  
+                num = num / 10; 
+                sum = sum * 10 + rem; 
+    
+            }
 
-        if (temp == sum) 
-        {
-            Console.WriteLine("Число палиндром");
-        }
+            if (temp == sum) 
+            {
+                Console.WriteLine("Число палиндром");
+            }
+            else
+            {
+                Console.WriteLine("Число не палиндром");
+            }
+
+        } 
         else
         {
-            Console.WriteLine("Число не палиндром");
+            Console.WriteLine("Вы ввели неверное число");
         }
-        Console.ReadLine();
+    }
+    task19(); 
